f2f4f5377370a86ee01ae10d6d7e4e9a63cfc1de
Sarabjeet Singh <sarabjeet.s@qikfox.com>
2024-06-05 18:50:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const RemoteButton = React.lazy(() => import("remoteapp/AppComponent"));
 
 function App() {
+  const [show, setshow] = React.useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main onClick={() => setshow((show) => !show)}>
+        This is the host application
+      </main>
+      <div
+        style={{
+          width: "50%",
+          height: "500px",
+          margin: "auto",
+          background: "gray",
+        }}
+      >
+        {show && (
+          <React.Suspense fallback="Loading...">
+            <RemoteButton />
+          </React.Suspense>
+        )}
+      </div>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { modules: false }],
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+            },
+          },
+          "url-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "hostapp",
+      remotes: {
+        remoteapp: "remoteapp@http://localhost:8080/remoteEntry.js",
+      },
+      // shared: ["react", "react-dom"],
+      shared: {
+        react: {
+          eager: false,
+        },
+      },
+    }),
+  ],
+};
